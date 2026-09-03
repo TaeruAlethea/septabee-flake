6bc9fd080e1f76ce22c68ea0305d908cc2f1fa84
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2026-09-03 15:09:41 -0400

    Wayland working

--- flake.nix
+++ flake.nix
@@ -25,12 +25,11 @@
       ncurses
       wayland
       libxkbcommon
-      kdePackages.wayland
     ];
     version = "yeet-44";
 
     septabee-pkg = pkgs.stdenv.mkDerivation {
-        name = "septabee";
+        pname = "septabee";
         version = version;
         src = pkgs.fetchurl {
           url = "https://septabee.nekoweb.org/important_stuff/SEPTABEE_DOWNLOADS/version_B/septabee_linux_B_T2.7z";
@@ -51,15 +50,21 @@
           runHook postUnpack
         '';
 
+        dontBuild = true;
+
         installPhase = ''
           runHook preInstall
-          mkdir -p "$out/bin" 
-          cp -r ./linux/* "$out/bin/"
+          
+          mkdir -p "$out/bin" "$out/lib/septabee"
+          cp -r ./linux/* "$out/lib/septabee"
+          ln -s "$out/lib/septabee/septabee" "$out/bin/septabee"
+          
           runHook postInstall
         '';
 
         postFixup = ''
           wrapProgram "$out/bin/septabee" \
+            --prefix LD_LIBRARY_PATH : "${nixpkgs.lib.makeLibraryPath depends}" \
             --chdir "$out" \
             --run "
               data_home=\"\''\${XDG_DATA_HOME:-\$HOME/.local/share}\"
@@ -68,6 +73,8 @@
               mkdir -p \"\$abi_dir\"
             "
         '';
+      
+        meta.mainProgram = "septabee";
     };
   in
   {
